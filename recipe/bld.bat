a52350353905bdd6d5b24a5d431e2f8411f3986f
seanyen <seanyen@microsoft.com>
2019-10-08 23:47:02 -0700

    Use Vcpkg recipe.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,19 @@
 setlocal EnableDelayedExpansion
 
 copy %RECIPE_DIR%\CMakeLists.txt .\CMakeLists.txt
+copy %RECIPE_DIR%\libtheora.def .\libtheora.def
 
 :: Make a build folder and change to it.
 mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-%LIBRARY_BIN%\cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE:STRING=Release .. 
+cmake ^
+    -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DBUILD_SHARED_LIBS=ON ^
+    .. 
 if errorlevel 1 exit 1
 
 :: Build!
