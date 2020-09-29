e2d00e4d548a30bbb50d90b8887176fa153605b9
Wolf Vollprecht <w.vollprecht@gmail.com>
2020-09-29 20:31:14 +0200

    regenerate autoconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,9 @@ export C_INCLUDE_PATH=${PREFIX}/include
 export LDFLAGS="-L${PREFIX}/lib"
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 
+rm ./config.sub
+./autogen.sh
+
 # -fforce-addr is not supported in clang
 if [ `uname` == Darwin ]; then
     sed -i.bak 's/-fforce-addr //g' ./configure
